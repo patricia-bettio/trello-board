0e615829f5c361bac6014b8d0a16d4875bcd2c17
Patricia Bettio <pfbettio@gmail.com>
2020-04-20 18:19:09 +0200

    basic style structure

---
+++ forms_trello.js
@@ -0,0 +1,1 @@
+//import { endpoint, apiKey} from ".modules/settings";
---
+++ modules/settings.js
@@ -0,0 +1,2 @@
+export const endpoint = "https://frontend2020-db3c.restdb.io/";
+export const apiKey = "5e95774d436377171a0c233c";
